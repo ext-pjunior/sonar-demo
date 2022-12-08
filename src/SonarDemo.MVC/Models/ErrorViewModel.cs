2029dac2d8790376af25a174e8ae5d0bdb75343c
ext.paulo.junior <ext.paulo.junior@zamp.com.br>
2022-12-08 14:08:24 -0300

    alterando o projeto mvc

---
+++ src/SonarDemo.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SonarDemo.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
